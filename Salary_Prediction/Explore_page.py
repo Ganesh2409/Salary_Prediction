a6186b3408b76a169a32fc0ca9f2c0ba7954c3da
Pinnamaneni Ganesh Chowdhary <99950535+Ganesh2409@users.noreply.github.com>
2023-12-16 06:58:50 +0530

    Update Explore_page.py

--- Salary_Prediction/Explore_page.py
+++ Salary_Prediction/Explore_page.py
@@ -1,5 +1,9 @@
 from pickle import TRUE
-import streamlit as st,pandas as pd,matplotlib.pyplot as plt,warnings,numpy as np
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
 warnings.filterwarnings('ignore')
 
 def remove_values(categories,cutoff):
